07f8d0daa6543b65f3d696fc55c84e3305eedc71
O Nacélio! <nd7058898@gmail.com>
2023-01-04 17:37:34 -0300

    imagens upadas apagas e informações da tabela

--- bd/controledeusuarios.sql
+++ bd/controledeusuarios.sql
@@ -47,11 +47,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `level`, `firstname`, `lastname`, `email`, `date_birth`, `user_password`, `gender`, `image`, `date_cadastre`) VALUES
-(17, 'user', 'AntÃ´nio', 'NacÃ©lio', 'nd7058898@gmail.com', '2005-04-29', '$2y$10$mRfzOx7H79roA7LBXn1ened0LAsTa8AQx6oWUESzOfI7xNIwFOhx2', 'male', 'uploaded-img/63b5dc59ddb81.jpg', '2023-01-04 18:23:35'),
-(16, 'admin', 'Admin', 'admin', 'administrador@admin.com', '2005-04-29', '$2y$10$dtYAMiwXzgYDcRPGd61YUedX9aUZZiQc17YcBwX8/05rT7zktvW0G', 'none', '.png', '2023-01-04 18:19:57'),
-(18, 'user', 'Samira', 'Lima', 'samira@gmail.com', '2005-11-28', '$2y$10$ORZM6wxEUDb.v76hcsVfFem6cvHd5361sfw/rva5BnzJymk34qnqa', 'female', 'uploaded-img/63b5dbc650aba.jpeg', '2023-01-04 18:24:54'),
-(19, 'user', 'KK', 'KK', 'KK@gmail.com', '1212-12-12', '$2y$10$0cXMJSwEhXK8aM4J.Kd9sO2C3UxhNa8nnAgG.mFrImLeSX.PPslge', 'none', 'uploaded-img/63b5da66ca68e.png', '2023-01-04 18:26:02');
-COMMIT;
+(0, 'admin', 'Admin', 'admin', 'administrador@admin.com', '2005-04-29', '$2y$10$dtYAMiwXzgYDcRPGd61YUedX9aUZZiQc17YcBwX8/05rT7zktvW0G', 'none', '.png', '2023-01-04 18:19:57');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
